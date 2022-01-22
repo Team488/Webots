db879ab5862650bea6a1349839fe350994e2c302
Alex Schokking <alexs@textio.com>
2022-01-22 12:45:21 -0800

    fix up existing protos for new coordinate system

---
+++ RobotServer/controllers/swerve_test/swerve_test.py
@@ -0,0 +1,38 @@
+"""swerve_test controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+wheel_motor = robot.getDevice('wheel_motor')
+direction_motor = robot.getDevice('direction_motor')
+wheel_motor.setPosition(float('inf'))
+direction_motor.setPosition(float('inf'))
+
+wheel_motor.setVelocity(0.5)
+direction_motor.setVelocity(0.1)
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
