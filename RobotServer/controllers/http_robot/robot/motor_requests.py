70995a0cfd11e36159a6859cd7ce479387abaf27
Alex Schokking <aschokking@gmail.com>
2022-02-15 19:36:50 -0800

    Virtual solenoid support (#70)
    * Add support for solenoids as motors
    
    * support virtual solenoid

--- RobotServer/controllers/http_robot/robot/motor_requests.py
+++ RobotServer/controllers/http_robot/robot/motor_requests.py
@@ -5,6 +5,11 @@ class MotorModes(enum.Enum):
     POWER = 0
     VELOCITY = 1
     POSITION = 2
+    VIRTUAL_SOLENOID = 3
+
+class SolenoidPositions(enum.Enum):
+    ON = 'on'
+    OFF = 'off'
 
 
 def apply_motor_requests(
@@ -31,6 +36,16 @@ def update_motors(device_map: dict, motor_requests: dict) -> None:
             motor.setVelocity(float(value * motor.getMaxVelocity()))
         elif mode == MotorModes.POSITION:
             motor.setPosition(float(value))
+        elif mode == MotorModes.VIRTUAL_SOLENOID:
+            target_position = SolenoidPositions[value.upper()]
+            # Set the max velocity that the positional pid can use
+            motor.setVelocity(motor.getMaxVelocity())
+            if target_position == SolenoidPositions.ON:
+                motor.setPosition(motor.getMaxPosition())
+            elif target_position == SolenoidPositions.OFF:
+                motor.setPosition(motor.getMinPosition())
+            else:
+                raise ValueError("Unknown solenoid position: " + target_position)
         elif mode == MotorModes.POWER:
             motor.setForce(float(value * motor.getMaxTorque()))
         else:
